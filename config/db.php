d5e8c3696fda4fabce757ec5a0967a59b721bef9
AlexisNox <atanova.sasha251201@gmail.com>
2022-10-30 16:33:18 +0600

    first commit

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1;port=8081;dbname=yii2_uib',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
